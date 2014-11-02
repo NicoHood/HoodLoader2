8707904d68b704a3db433610c68a0539fdd31476
Nico <nico-2006@web.de>
2014-11-02 01:40:00 +0100

    Updated License

--- Config/LUFAConfig.h
+++ Config/LUFAConfig.h
@@ -28,6 +28,26 @@
   this software.
 */
 
+/*
+Copyright(c) 2014 NicoHood
+See the readme for credit to other people.
+
+This file is part of Hoodloader2.
+
+Hoodloader2 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Hoodloader2 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Hoodloader2.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /** \file
  *  \brief LUFA Library Configuration Header File
  *
