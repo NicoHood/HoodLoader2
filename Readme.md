8fafd5edd3ba057aa3f60dc7268ea8604b381b46
Nico <nico-2006@web.de>
2015-02-14 12:39:14 +0100

    Updated drivers
    Now a separate driver for each usb core for better pc side recognition
    if the reports/core change.
    
    Also corrected wrong PIDs in boards.txt

--- Readme.md
+++ Readme.md
@@ -58,13 +58,17 @@ Version History
 ===============
 ```
 2.0.4 Release (xx.xx.2015)
-* added HID-Project 2.2 compatibility
-* added boards.txt for Uno/Mega + HID-Bridge
+* Added HID-Project 2.2 compatibility
+* Added boards.txt for Uno/Mega + HID-Bridge
+* Improved Uno/Mega uploading (no double tab needed any more)
+* Updated USB drivers for better PC side recognition
+  (Each USB core has a different PID + Windows driver)
+* Fixed wrong PIDs in boards.txt
 
 2.0.3 Release (28.01.2015)
-* added HID Project 2.1 board definition compatibility
-* fixed usb flag pass via boards.txt (instead of pins_arduino.h)
-* fixed 16u2 4 pin header pinout picture
+* Added HID Project 2.1 board definition compatibility
+* Fixed usb flag pass via boards.txt (instead of pins_arduino.h)
+* Fixed 16u2 4 pin header pinout picture
 * DFU burn bootloader fix
 * HoodLoader2.0.3 released (minor firmware CDC identifier fix)
 * Added 32u2 bootloader to the installation sketch as well
--- avr/variants/standard_custom/pins_arduino.h
+++ avr/variants/standard_custom/pins_arduino.h
@@ -49,7 +49,7 @@ You have to enable the specific hid apis on your own then, also the keyboard led
 HoodLoader2 sketches are limited to a maximum 15 byte HID report.
 Dont use more than that. (for a Leonardo/Micro it is 63 bytes).
 
-Arduino Uno/Mega's main mcu cannot have a specified HID descriptor,
+Arduino Uno/Mega's main MCU cannot have a specified HID descriptor,
 edit the HoodLoader2 file for the 16u2 instead.
 It is more important that the HID APIs are the same on both devices.
 */
@@ -81,10 +81,8 @@ It is more important that the HID APIs are the same on both devices.
 // use this to enable the Keyboard Led functions
 //#define HID_KEYBOARD_LEDS_ENABLED
 
-// add your custom report here:
-#define EXTERN_HID_REPORT \
-HID_REPORT_KEYBOARD_KEYS(HID_REPORTID_KEYBOARD), \
-HID_REPORT_MOUSE(HID_REPORTID_MOUSE)
+// flag that you are using a custom hid report descriptor
+#define EXTERN_HID_REPORT
 
 // activate your custom HID-APIs here:
 #define HID_MOUSE_API_ENABLE
@@ -95,74 +93,6 @@ HID_REPORT_MOUSE(HID_REPORTID_MOUSE)
 //#define HID_SYSTEMCONTROL_API_ENABLE
 //#define HID_GAMEPAD_API_ENABLE
 //#define HID_ENABLE_ALL_APIS // enables all of the ones above
-
-
-//// 2nd example with a custom Joystick report (the Gamepad API wont work anymor since the report is different)
-//#define EXTERN_HID_REPORT \ /* Gamepad with 32 buttons and 10 axis*/ \
-//0x05, 0x01,							/* USAGE_PAGE (Generic Desktop) */ \
-//0x09, 0x04,							/* USAGE (Joystick) */ \
-//0xa1, 0x01,							/* COLLECTION (Application) */ \
-//0x85, HID_REPORTID_GAMEPAD,					/*   REPORT_ID */ \
-///* 32 Buttons */ \
-//0x05, 0x09,							/*   USAGE_PAGE (Button) */ \
-//0x19, 0x01,							/*   USAGE_MINIMUM (Button 1) */ \
-//0x29, 0x20,							/*   USAGE_MAXIMUM (Button 32) */ \
-//0x15, 0x00,							/*   LOGICAL_MINIMUM (0) */ \
-//0x25, 0x01,							/*   LOGICAL_MAXIMUM (1) */ \
-//0x75, 0x01,							/*   REPORT_SIZE (1) */ \
-//0x95, 0x20,							/*   REPORT_COUNT (32) */ \
-//0x81, 0x02,							/*   INPUT (Data,Var,Abs) */ \
-///* 10 8bit Axis */ \
-//0x05, 0x01,							/*   USAGE_PAGE (Generic Desktop) */ \
-//0xa1, 0x00,							/*   COLLECTION (Physical) */ \
-//0x09, 0x30,							/*     USAGE (X) */ \
-//0x09, 0x31,							/*     USAGE (Y) */ \
-//0x09, 0x33,							/*     USAGE (Rx) */ \
-//0x09, 0x34,							/*     USAGE (Ry) */ \
-//0x09, 0x32,							/*     USAGE (Z) */ \
-//0x09, 0x35,							/*     USAGE (Rz) */ \
-//0x09, 0x36,							/*	   Usage (Slider) */ \
-//0x09, 0x36,							/*	   Usage (Slider) */ \
-//0x09, 0x36,							/*	   Usage (Slider) */ \
-//0x09, 0x36,							/*	   Usage (Slider) */ \
-//0x15, 0x80,							/*     LOGICAL_MINIMUM (-128) */ \
-//0x25, 0x7F,							/*     LOGICAL_MAXIMUM (127) */ \
-//0x75, 0x08,							/*     REPORT_SIZE (8) */ \
-//0x95, 0x0A,							/*     REPORT_COUNT (10) */ \
-//0x81, 0x02,							/*     INPUT (Data,Var,Abs) */ \
-//0xc0,								/*   END_COLLECTION */ \
-//0xc0								/* END_COLLECTION */
-//
-//// activate your custom HID-APIs here:
-////#define HID_MOUSE_API_ENABLE
-////#define HID_MOUSE_ABSOLUTE_API_ENABLE
-////#define HID_KEYBOARD_API_ENABLE
-////#define HID_RAWHID_API_ENABLE
-////#define HID_CONSUMERCONTROL_API_ENABLE
-////#define HID_SYSTEMCONTROL_API_ENABLE
-////#define HID_GAMEPAD_API_ENABLE
-////#define HID_ENABLE_ALL_APIS // enables all of the ones above
-
-
-/*
-You can use the pre defined reports as well.
-Keep in mind that changing the ids or reports might confuse your OS.
-Then you might need to reinstall all drivers or change the USB PID.
-Mouse Abs only works with a System report together.
-Gamepad and Mouse Abs dont work together.
-Gamepads can causes some recognition problems with some OS.
-RAW HID seems to not works properly in a multireport.
-Currently available pre defined reports :
-*/
-
-//HID_REPORT_KEYBOARD_LEDS(HID_REPORTID_KEYBOARD),
-//HID_REPORT_KEYBOARD_KEYS(HID_REPORTID_KEYBOARD),
-//HID_REPORT_MOUSE(HID_REPORTID_MOUSE),
-//HID_REPORT_MOUSE_ABSOLUTE(HID_REPORTID_MOUSE_ABSOLUTE),
-//HID_REPORT_RAWHID(HID_REPORTID_RAWHID),
-//HID_REPORT_CONSUMERCONTROL(HID_REPORTID_CONSUMERCONTROL),
-//HID_REPORT_SYSTEMCONTROL(HID_REPORTID_SYSTEMCONTROL),
-//HID_REPORT_GAMEPAD(HID_REPORTID_GAMEPAD),
 
 #else
 #error Please select automatic or custom hid report in the pins_arduino.h!
