925a398bf078df16e31f535d4a2e57dd2418e8a9
NicoHood <NicoHood@users.noreply.github.com>
2015-08-09 10:20:17 +0200

    32u4 fixes

---
+++ avr/variants/micro_HL2/pins_arduino.h
@@ -0,0 +1,26 @@
+/*
+Copyright(c) 2014-2015 NicoHood
+See the readme for credit to other people.
+
+This file is part of Hoodloader2.
+
+Hoodloader2 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Hoodloader2 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Hoodloader2.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <../../variants/micro/pins_arduino.h>
+
+// Bootkey position (8bit)
+#define MAGIC_KEY 0x77
+#define MAGIC_KEY_POS (uint8_t *)(RAMEND)
+#define NO_MAGIC_KEY_BACKUP
